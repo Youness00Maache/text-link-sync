61a406405a82fd681f74aadd3ed14b7aaf458029
Youness00Maache <80402935+Youness00Maache@users.noreply.github.com>
2025-09-05 11:43:11 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// TextLinker Server with Chunked Upload Support
+// TextLinker Server with Chunked Upload Support2
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
